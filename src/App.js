120cbbaa075968355bc7fc8245268aeb751451ae
Mayank Pandey <mp.pandey86@gmail.com>
2020-07-12 16:56:32 +0530

    Changed particle numbers

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ import './App.css';
 const parameters = {
   particles: {
     number: {
-      value: 30,
+      value: 70,
       density: {
         enable: true,
-        value_area: 800
+        value_area: 500
       }
     }
   }
